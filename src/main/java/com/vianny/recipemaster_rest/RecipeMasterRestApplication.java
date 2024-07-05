696bb9514be2d51bffa0c62876b885c37b76b2c1
ilya Picenko <pitsenko.ilya@mail.ru>
2024-07-05 17:36:06 +0300

    init project

---
+++ src/main/java/com/vianny/recipemaster_rest/RecipeMasterRestApplication.java
@@ -0,0 +1,13 @@
+package com.vianny.recipemaster_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeMasterRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeMasterRestApplication.class, args);
+    }
+
+}
